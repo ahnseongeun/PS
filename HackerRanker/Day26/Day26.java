979d9295a325fe540c67e1685945850ebe872391
seongeun ahn <ast3138@hanmail.net>
2020-12-11 13:39:29 +0900

    2020-12-11 Day26

--- HackerRanker/Day26/Day26.java
+++ HackerRanker/Day26/Day26.java
@@ -5,6 +5,24 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Day26 {
+    private static boolean checkDay(int return_d, int due_d) {
+        if(return_d>due_d)
+            return true;
+        return false;
+    }
+
+    private static boolean checkMonth(int return_m,int due_m) {
+        if(return_m>due_m)
+            return true;
+        return false;
+    }
+
+    private static boolean checkYear(int return_y,int due_y) {
+        if(return_y>due_y)
+            return true;
+        return false;
+    }
+
     public static void main(String[] args) throws Exception{
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         BufferedReader input=new BufferedReader(new InputStreamReader(System.in));
@@ -17,35 +35,27 @@ public class Day26 {
         int due_m=Integer.parseInt(st.nextToken());
         int due_y=Integer.parseInt(st.nextToken());
         int total=0;
-        if(return_y<due_y){
+
+        if(checkYear(return_y,due_y)){
+            total=10000;
             System.out.println(total);
-        }else if(return_y==due_y){
-            if(return_m<due_m){
-                System.out.println(total);
-            }else if(return_m==due_m){
-                if(return_d<due_d){
+        }else{
+            if(return_y-due_y<0) {
+                System.out.println(0);
+            }else {
+                if (checkMonth(return_m, due_m)) {
+                    total = (return_m - due_m) * 500;
                     System.out.println(total);
-                }else{
-                    total=return_d-due_d;
-                    System.out.println(total);
-                }
-            }else{
-                if(return_d<due_d){
-                    System.out.println(total);
-                }else{
-
-                }
-            }
-        }else{//반납년도가 만료년도보다 빠를경우 년기준
-            if(return_m<due_m){//반납월이 만료월보다 빠를경우 월기준
-                System.out.println(total);
-            }else{
-                if(return_d<due_d){//반납월이 만료월보다 빠를경우 월기준
-                    System.out.println(total);
-                }else{
-
+                } else {
+                    if (checkDay(return_d, due_d)) {
+                        total = (return_d - due_d) * 15;
+                        System.out.println(total);
+                    } else {
+                        System.out.println(0);
+                    }
                 }
             }
         }
+
     }
 }

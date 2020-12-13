6c972bc5a8ba0b3a87f374b9444bf5f07cc2be3b
ahnseongeun <ast3138@naver.com>
2020-12-13 20:08:06 +0900

    2020-12-13 쓰레드와비동기화 테스트 추후 보완

--- JAVA/ThreadAndAsynchronization.java
+++ JAVA/ThreadAndAsynchronization.java
@@ -1,7 +1,7 @@
 package JAVA;
 
 class sharedData {
-    int n=4;
+    int n=10;
     int count=1;
     boolean ready=false;
 }
@@ -9,35 +9,33 @@ class ReaderThread extends Thread {
     private int result;
     sharedData data;
 
-    public synchronized void share(sharedData data)
+    synchronized int share(sharedData data)
     {
         this.data=data;
+        return data.count++;
     }
     public void run() {
         while(!data.ready) {
-            System.out.println(data.count+"번 쓰레드 실행");
-            share(data);
-            setResult(factorial(data.n,data.count));
+            System.out.println(share(data)-4+"번 쓰레드 실행");
+            setResult(factorial(data.n));
             data.n=(data.n)-1;
             data.ready=true;
-            System.out.println(data.count+"번 쓰레드 종료");
-            data.count++;
         }
         data.ready=false;
     }
-    public static int factorial(int n,int count) {
+    public int factorial(int n) {
         if(n==1) {
             //System.out.println(count);
             return n ;
         }
         else
-            return n*factorial(n-1,count);
+            return n*factorial(n-1);
     }
 
     public int getResult() {
         return result;
     }
-    public  void setResult(int result) {
+    public void setResult(int result) {
         this.result = result;
     }
 
@@ -49,49 +47,38 @@ public class ThreadAndAsynchronization {
 
     public static void main(String[] args) {
 
-
+        sharedData a= new sharedData(); //공유 데이터
         ReaderThread thread1=new ReaderThread();
         ReaderThread thread2=new ReaderThread();
         ReaderThread thread3=new ReaderThread();
         ReaderThread thread4=new ReaderThread();
-        sharedData a= new sharedData(); //공유 데이터
-
-        thread1.data=a;
+        thread1.share(a);
+        thread2.share(a);
+        thread3.share(a);
+        thread4.share(a);
+//        thread1.data=a;
+//        thread2.data=a;
+//        thread3.data=a;
+//        thread4.data=a;
         thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+        StringBuilder sb=new StringBuilder();
         try {
             thread1.join();
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        thread2.data=a;
-        thread2.start();
-        try {
+            sb.append("1");
             thread2.join();
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        thread3.data=a;
-        thread3.start();
-        try {
+            sb.append("2");
             thread3.join();
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        thread4.data=a;
-        thread4.start();
-        try {
+            sb.append("3");
             thread4.join();
+            sb.append("4");
         } catch (InterruptedException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-
-
-
-
+        System.out.println(sb.toString());
         System.out.println("1번째 쓰레드결과값: "+thread1.getResult());
         System.out.println("2번째 쓰레드결과값: "+thread2.getResult());
         System.out.println("3번째 쓰레드결과값: "+thread3.getResult());

6e834e2d11b88adc2567210ea3be8659b2d2dea2
ahnseongeun <ast3138@naver.com>
2021-01-02 14:15:52 +0900

    2020-1-2 일이 될때 까지 2

--- 이것이코딩테스트다_BOOK/그리디/일이될때까지.java
+++ 이것이코딩테스트다_BOOK/그리디/일이될때까지.java
@@ -5,14 +5,18 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * 그리디인 배수를 이용해서 하는 것이 압도적으로 속도가 빠르다.
+ */
 public class 일이될때까지{
     public static void main(String[] args) throws Exception{
         BufferedReader input=new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st=new StringTokenizer(input.readLine());
-        int n=Integer.parseInt(st.nextToken());
+        long n=Long.parseLong(st.nextToken());
         int m=Integer.parseInt(st.nextToken());
-        int target=n;
+        long target=n;
         int cnt=0;
+        long currentTime=System.currentTimeMillis();
         while(true){
             if(target==1)
                 break;
@@ -23,6 +27,29 @@ public class 일이될때까지{
                 target-=1;
             }
         }
-        System.out.println(cnt);
+        System.out.println("first"+cnt);
+        long finishTime=System.currentTimeMillis();
+        System.out.println("Total time1: "+(finishTime-currentTime));
+        long result=0;
+        currentTime=System.currentTimeMillis();
+        while(true){
+            //나눠지지 않는 수만큼 더하는 로직{
+            target=(n/m)*m; //몫계산
+            result += (n-target); //타켓에서 몫을 뺀만 큼 더 해준다.
+            n = target; //새로운 몫으로 교체
+            //}
+
+            if(n<m) //몫이 나누는 수보다 작다면 반복문 교체
+                break;
+
+            //n을 나누는 로직{
+            n /= m;
+            result += 1;
+            //}
+        }
+        result += (n-1);
+        System.out.println("second"+result);
+        finishTime = System.currentTimeMillis();
+        System.out.println("Total time2: "+(finishTime-currentTime));
     }
 }

a2928dc671540af5abaeadd1bf84c373a268411a
ahnseongeun <ast3138@naver.com>
2021-01-04 10:21:53 +0900

    이것이 코딩테스트이다 merge

---
+++ CodingTest/이투스/TESTS.java
@@ -0,0 +1,10 @@
+package CodingTest.이투스;
+
+import java.util.ArrayList;
+
+public class TESTS {
+    public static void main(String[] args){
+        double a= Math.ceil(9/4.0);
+        System.out.println(a);
+    }
+}

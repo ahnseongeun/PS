8cd829c3d288cf8f54ad9c5944d5dd502bf0778b
ahnseongeun <ast3138@naver.com>
2021-02-26 02:35:54 +0900

    2020-2-26 보물섬

--- DataStructure/Math/토너먼트_1057.java
+++ DataStructure/Math/토너먼트_1057.java
@@ -1,4 +1,4 @@
-package DataStructure.진행중인문제;
+package DataStructure.Math;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -6,6 +6,10 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+/**
+ * 입력받은 두 인덱스를 /2 하면서 같이질때 while문을 탈출 한다
+ * 같은 경우를 빼줘야 하기 때문에 -1 해준다.
+ */
 public class 토너먼트_1057 {
 
     public static void main(String[] args) throws Exception{
@@ -16,5 +20,18 @@ public class 토너먼트_1057 {
         int kim = Integer.parseInt(st.nextToken());
         int im = Integer.parseInt(st.nextToken());
 
+        int count = 1;
+        while(true){
+
+            if(kim == im) {
+                count--;
+                break;
+            }
+            kim = (int) Math.round(kim / 2.0);
+            im = (int) Math.round(im / 2.0);
+            //System.out.println(kim + " " + im);
+                count++;
+        }
+        System.out.println(count);
     }
 }
---
+++ DataStructure/진행중인문제/보물섬_2589.java
@@ -0,0 +1,82 @@
+package DataStructure.진행중인문제;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/**
+ * 문제접근법 - 완전탐색을 했을 경우 시간 복잡도가 통과하는지 확인하는지가 첫번째 문제
+ * 가로 50, 세로 50 이기 때문에 최대 2500의 배열의 원소의 수
+ * 각 원소의 자리마다 50*50번의 탐색
+ * 즉, 최악의 경우 2500 * 2500의 경우의 대략 6백만번의 연산 발생생 */
+public class 보물섬_2589 {
+
+    private static int n;
+    private static int m;
+    private static char arr[][];
+    private static boolean visited[][];
+    private static int[] dx = {0,0,-1,1}; //상하좌우
+    private static int[] dy = {-1,1,0,0};
+
+    private static int bfs(int y, int x) {
+
+        Queue<int[]> q = new LinkedList<>();
+        int max = 0;
+        q.add(new int[]{x,y,0});
+        visited[y][x] = true;
+
+        while(!q.isEmpty()){
+            int[] tmp = q.poll();
+            x = tmp[0];
+            y = tmp[1];
+            int count = tmp[2];
+
+            max = Math.max(max,count);
+
+            for(int i = 0; i < 4; i++){
+                int next_x = x + dx[i];
+                int next_y = y + dy[i];
+
+                if(next_x < 0 || next_y < 0 || next_x >= m || next_y >= n )
+                    continue;
+
+                if(visited[next_y][next_x] || arr[next_y][next_x] == 'W')
+                    continue;
+
+                visited[next_y][next_x] = true;
+                //System.out.println(next_x + " " + next_y + " " + count);
+                q.add(new int[]{next_x, next_y, count + 1});
+            }
+        }
+        //System.out.println(max);
+        return max;
+    }
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(input.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new char[n][m];
+
+        for(int i = 0; i < n; i++){
+            String str = input.readLine();
+            for(int j = 0; j < m; j++ )
+                arr[i][j] = str.charAt(j);
+        }
+
+        int max = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(arr[i][j] == 'W')
+                    continue;
+                visited = new boolean[n][m];
+                max = Math.max(max,bfs(i,j));
+            }
+        }
+        System.out.println(max);
+    }
+
+}

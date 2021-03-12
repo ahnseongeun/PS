035177759033df815e37e41c1c8e9b0b849c876c
ahnseongeun <ast3138@naver.com>
2021-03-13 02:30:37 +0900

    2020-3-13 부분수열의합

---
+++ DataStructure/진행중인문제/부분수열의합_14225.java
@@ -0,0 +1,32 @@
+package DataStructure.진행중인문제;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// 1 2 2 7
+public class 부분수열의합_14225 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(input.readLine());
+        StringTokenizer st = new StringTokenizer(input.readLine());
+        int arr[] = new int[n];
+        for(int i = 0; i < n; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(arr);
+
+        //(bfs)
+
+
+        //(그리디)가장 작은수로 초기화
+        int result = 1;
+
+        for(int i = 0; i < arr.length; i++){
+            if(result < arr[i])
+                break;
+            result += arr[i];
+        }
+        System.out.println(result);
+    }
+}
---
+++ JAVA/조건변수/importTest.java
@@ -0,0 +1,23 @@
+package JAVA.조건변수;
+
+import java.util.*;
+
+public class importTest {
+//    public static int test(){
+//
+//    }
+//    public static void main(String[] args){
+//        HashMap<String,String> map = new HashMap<>();
+//        int arr[] = new int[5];
+//        System.out.println(arr.length);
+//        LinkedList<Integer> list = new LinkedList<>();
+//        list.size()
+//        Arrays.sort();
+//        Collections.sort();
+//        String str = "test";
+//        str.length();
+//        str.charAt();
+//        StringBuffer sb = new StringBuffer();
+//        sb.length()
+//    }
+}

382713bf37341f1802877e5d1502d7c71fe84f4d
ahnseongeun <ast3138@naver.com>
2021-03-30 14:32:09 +0900

    2020-3-30 인구이동

--- CodingTest/소마2/RETEST3.java
+++ CodingTest/소마2/RETEST3.java
@@ -75,24 +75,34 @@ public class RETEST3 {
             }
         }
 
-        searchMap1(1,1,n,n,0);
-        System.out.println(max);
+        System.out.println(searchMap(1,n,1,n));
     }
 
     private static int searchMap(int y, int y_size, int x, int x_size) {
         //if(dy[y][y_size][x][x_size] != 0) return dy[y][y_size][x][x_size];
-        if(y == y_size && x == x_size) return 0;
+        if(y == y_size && x == x_size) {
+            System.out.println(y);
+            System.out.println(y_size);
+            System.out.println(x);
+            System.out.println(x_size);
+            System.out.println("test");
+            return 0;
+        }
 
         int res = 0;
 
-        if(y != y_size){
+        //이 문제의 핵심은 재귀를 할 때 경계값을 어떻게 설정하는가 이다.
+        //x == x_size라면 재귀를 멈춘다. , y == y_size라면 재귀를 멈춘다.
+        //y값이 y_size와 같다는 의미가 무엇인가?? 시작과 끝이 같아서 더이상 재귀를 진행하면 안된다는 의미이다.
+        if(y != y_size){ //y값이 y_size와 같다는 의미가 무엇인가??
             int m = (y + y_size) / 2;
             // 위쪽에서 최대 값 구하고 , 아래로 재귀
             res = Math.max(res, get_max(y, m , x, x_size) + searchMap(m + 1, y_size, x, x_size));
             // 아래쪽에서 최대 값 구하고, 위로 재귀
             res = Math.max(res, searchMap(y, m , x, x_size) + get_max(m + 1, y_size, x, x_size));
         }
-        if(x != x_size){
+
+        if(x != x_size){ //x값이 x_size와 같다는 의미가 무엇인가??
             int m = (x + x_size) / 2;
             // 왼쪽에서 최대 값 구하고, 오른쪽으로 재귀
             res = Math.max(res, get_max(y, y_size , x, m) + searchMap(y, y_size, m + 1, x_size));
---
+++ DataStructure/진행중인문제/인구이동.java
@@ -0,0 +1,112 @@
+package DataStructure.진행중인문제;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+/*
+2 20 50
+50 30
+20 40
+ */
+public class 인구이동 {
+
+    private static int country[][];
+    private static int dx[] ={0,0,-1,1}; //상,하,좌,우
+    private static int dy[] ={-1,1,0,0};
+    private static int check[][];
+    private static int count = 0;
+    private static int sum = 0;
+    private static boolean flag;
+    private static int n,l,r;
+
+    private static boolean moveCountryCheck() {
+        int cnt = 0;
+        flag = false;
+        for(int i = 0; i < n; i++){
+            for(int j = 0 ; j < n; j++){
+                if(check[i][j] == 0){
+                    check[i][j] = ++cnt; //연합 번호
+                    sum = country[i][j];
+                    count = 1;
+                    dfs(i,j,cnt);
+                    int avg = sum / count;
+                    for(int k = 0; k < n; k++){
+                        for(int t = 0; t < n; t++){
+                            if(check[k][t] == cnt)
+                                country[k][t] = avg;
+                        }
+                    }
+                }
+            }
+        }
+        return flag;
+    }
+
+    private static void dfs(int y, int x, int cnt) {
+
+        for(int i = 0; i < 4; i++){
+            int next_x = x + dx[i];
+            int next_y = y + dy[i];
+
+            if(next_x < 0 || next_y < 0 || next_x >=n || next_y >= n || check[next_y][next_x] != 0)
+                continue;
+
+            if(Math.abs(country[y][x]-country[next_y][next_x]) >= l && Math.abs(country[y][x]-country[next_y][next_x]) <= r){
+                check[next_y][next_x] = cnt;
+                sum += country[next_y][next_x];
+                dfs(next_y,next_x,cnt);
+                count ++;
+                flag = true;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(input.readLine());
+        n = Integer.parseInt(st.nextToken());
+        l = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+
+        country = new int[n][n];
+
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(input.readLine());
+            for(int j = 0; j < n; j++){
+                country[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        check = new int[n][n];
+        int total = 0;
+        while(moveCountryCheck()) {
+            //display();
+            check = new int[n][n];
+            total++;
+        }
+        System.out.println(total);
+    }
+
+    private static void display() {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                sb.append(check[i][j] +" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+
+        sb = new StringBuilder();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                sb.append(country[i][j] +" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}

47d086f3e273e30a3e497c575e8628e3d4b606cc
ahnseongeun <ast3138@naver.com>
2021-04-02 21:32:58 +0900

    2020-4-1 나무제테크

--- DataStructure/DP/가장긴증가하는부분수열2_12015.java
+++ DataStructure/DP/가장긴증가하는부분수열2_12015.java
@@ -1,30 +1,42 @@
-package DataStructure.진행중인문제;
+package DataStructure.DP;
+
+import org.w3c.dom.ls.LSOutput;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-/**
- * 10 20 1 2 3 4
+/*
+6
+10 20 10 30 20 50
+6
+8 5 1 10 5 9
  */
-public class 가장긴증가하는부분수열3_12738 {
+public class 가장긴증가하는부분수열2_12015 {
+
+    //private static int arr[];
+
+
     public static void main(String[] args) throws Exception{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(input.readLine());
-
         ArrayList<Integer> list = new ArrayList<>();
-        list.add(-1000000001);
+        list.add(0);
         StringTokenizer st = new StringTokenizer(input.readLine());
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++) {
+            //value에 현재 비교해야 할 값 넣기
+            //StringBuilder sb = new StringBuilder();
             int value = Integer.parseInt(st.nextToken());
             int start = 0;
             int end = list.size() - 1;
+            //sb.append(end +"\n");
 
-            if(list.get(end) < value){
+            if (list.get(end) < value) { //마지막 값보다 value가 더 크다면 list에 추가
                 list.add(value);
-            }else{
+            } else{
                 int mid = 0;
                 while(start < end){
                     mid = (start + end) / 2;
@@ -36,11 +48,10 @@ public class 가장긴증가하는부분수열3_12738 {
                 }
                 list.set(end,value);
             }
+            //list.forEach(integer -> sb.append(integer+" "));
+            //System.out.println(sb);
         }
         System.out.println(list.size() - 1);
-        StringBuilder sb = new StringBuilder();
-        for(int i = 1; i < list.size(); i++)
-            sb.append(list.get(i)+" ");
-        System.out.println(sb);
+
     }
 }
--- DataStructure/DP/가장긴증가하는부분수열3_12738.java
+++ DataStructure/DP/가장긴증가하는부분수열3_12738.java
@@ -1,42 +1,30 @@
-package DataStructure.진행중인문제;
-
-import org.w3c.dom.ls.LSOutput;
+package DataStructure.DP;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-/*
-6
-10 20 10 30 20 50
-6
-8 5 1 10 5 9
+/**
+ * 10 20 1 2 3 4
  */
-public class 가장긴증가하는부분수열2_12015 {
-
-    //private static int arr[];
-
-
+public class 가장긴증가하는부분수열3_12738 {
     public static void main(String[] args) throws Exception{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(input.readLine());
+
         ArrayList<Integer> list = new ArrayList<>();
-        list.add(0);
+        list.add(-1000000001);
         StringTokenizer st = new StringTokenizer(input.readLine());
 
-        for(int i = 0; i < n; i++) {
-            //value에 현재 비교해야 할 값 넣기
-            //StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++){
             int value = Integer.parseInt(st.nextToken());
             int start = 0;
             int end = list.size() - 1;
-            //sb.append(end +"\n");
 
-            if (list.get(end) < value) { //마지막 값보다 value가 더 크다면 list에 추가
+            if(list.get(end) < value){
                 list.add(value);
-            } else{
+            }else{
                 int mid = 0;
                 while(start < end){
                     mid = (start + end) / 2;
@@ -48,10 +36,11 @@ public class 가장긴증가하는부분수열2_12015 {
                 }
                 list.set(end,value);
             }
-            //list.forEach(integer -> sb.append(integer+" "));
-            //System.out.println(sb);
         }
         System.out.println(list.size() - 1);
-
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i < list.size(); i++)
+            sb.append(list.get(i)+" ");
+        System.out.println(sb);
     }
 }

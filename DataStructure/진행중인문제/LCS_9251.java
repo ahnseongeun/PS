f49ee5fe2ebd66088e06d4a252f751bab3ee2251
ahnseongeun <ast3138@naver.com>
2021-04-20 23:55:11 +0900

    2020-4-20 LCS

--- DataStructure/진행중인문제/LCS_9251.java
+++ DataStructure/진행중인문제/LCS_9251.java
@@ -18,11 +18,11 @@ public class LCS_9251 {
         StringBuilder sb = new StringBuilder();
         String str1 = input.readLine();
         String str2 = input.readLine();
-//        if(str1.length() > str2.length()) {
-//            String str3 = str2;
-//            str2 = str1;
-//            str1 = str3;
-//        }
+        if(str1.length() > str2.length()) {
+            String str3 = str2;
+            str2 = str1;
+            str1 = str3;
+        }
         int dp[][] = new int[str1.length() + 1][str2.length() + 1];
 
         int max = 0;
@@ -30,9 +30,9 @@ public class LCS_9251 {
             for(int j = 1; j <= str2.length(); j++){
                 if(str1.charAt(i - 1) == str2.charAt(j - 1)){
                     //dp[i][j] = Math.max(dp[i][j - 1],dp[i - 1][j] + 1);
-                    //이곳을 최소값으로 해줘야하는 이유는? 그럼 이건?? 왜 최소인데??
-                    //2차원 배열이기 때문에 하나의 행에는 최대 + 1까지 만 허용된다.
-                    dp[i][j] = Math.min(dp[i][j - 1],dp[i - 1][j]) + 1;
+                    //dp[i - 1][j] 과 dp[i][j - 1]은 이미 현재 값에 영향을 받을 값이기 때문에 아무런 영향을 받지 않는
+                    //dp[i - 1][j - 1] + 1을 해줘야 한다.
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
                 }else{
                     //이곳을 최대값으로 해줘야하는 이유는? 여태까지 것중에 최대
                     dp[i][j] = Math.max(dp[i][j - 1],dp[i - 1][j]);

faf7a8afd2cb8bb837ca1b5dff331ea52945259e
ahnseongeun <ast3138@naver.com>
2021-05-15 00:54:43 +0900

    2021-5-14 괄호제거

--- DataStructure/진행중인문제/괄호제거_2800.java
+++ DataStructure/진행중인문제/괄호제거_2800.java
@@ -2,44 +2,81 @@ package DataStructure.진행중인문제;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.*;
 
 /*
 (0/(0))
+-------
+(0/0)
+0/(0)
+0/0
+ */
+
+/**
+ * 정답 중복제거
  */
 public class 괄호제거_2800 {
 
     private static String str;
+    private static ArrayList<int[]> pair;
     private static boolean visited[];
-    private static StringBuilder result = new StringBuilder();;
+    private static boolean check[];
+    private static Set<String> result;
+
+    private static void getPair() {
+
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch == '(') stack.add(i);
+            if(ch == ')') pair.add(new int[]{stack.pop(),i});
+        }
+    }
+
+    private static void erase(int idx) {
+
+        if(idx == pair.size()){
+            Arrays.fill(check,false);
+            boolean flag = false;
+            for(int i = 0; i < visited.length; i++){
+                if(visited[i]){
+                    int[] tmp = pair.get(i);
+                    check[tmp[0]] = true;
+                    check[tmp[1]] = true;
+                    flag = true;
+                }
+            }
+
+            if(!flag)
+                return;
+
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < check.length; i++)
+                if(!check[i]) sb.append(str.charAt(i));
+            result.add(sb.toString());
+            return;
+        }
+
+        visited[idx] = true;
+        erase(idx + 1);
+        visited[idx] = false;
+        erase(idx + 1);
+    }
 
     public static void main(String[] args) throws Exception{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         str = input.readLine();
-        visited = new boolean[str.length()];
-        erase(0,0);
-        System.out.println(result);
+        pair = new ArrayList<>();
+        getPair();
+        visited = new boolean[pair.size()];
+        check = new boolean[str.length()];
+        result = new TreeSet<>();
+        erase(0);
+        StringBuilder sb = new StringBuilder();
+        for(String str : result)
+            sb.append(str).append("\n");
+        System.out.println(sb);
     }
-
-    private static void erase(int idx, int pair) {
-        //idx가 str.length()와 같다면 return
-        if(idx == str.length()){
-            if(pair == 0){
-
-            }
-            return;
-        }
-
-        if(str.charAt(idx) == '('){
-            erase(idx + 1, pair);
-            visited[idx] = true;
-            erase(idx + 1, pair + 1);
-        }else if(str.charAt(idx) == ')'){
-            erase(idx + 1, pair);
-            visited[idx] = true;
-            erase(idx + 1, pair - 1);
-        }else{
-            erase(idx + 1, pair);
-        }
-
-    }
+    
 }

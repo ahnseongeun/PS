9776af64a7f16702b8d3b30d3909852b7a5177eb
ahnseongeun <ast3138@naver.com>
2021-05-16 17:05:24 +0900

    2021-5-16 아기상어 미완성

--- DataStructure/진행중인문제/아기상어_16236.java
+++ DataStructure/진행중인문제/아기상어_16236.java
@@ -1,6 +1,7 @@
 package DataStructure.진행중인문제;
 
 import java.io.BufferedReader;
+import java.io.EOFException;
 import java.io.InputStreamReader;
 import java.util.*;
 
@@ -38,116 +39,159 @@ N*N 크기
 0 0 0
 0 9 0
  */
-
-/**
- * 1. 물고기의 위치를 저장한다.
- * 2. 물고기의 위치를 점검해서 조건에 맞는 위치를 찾는다.
- * 3. 현재 상어의 위치에서 물고기의 위치까지 최단 거리를 찾는다.
- * 4. 상어의 위치를 초기화 하고 1번 반복
+/*
+물고기 M, 한칸에 물고기 최대 1마리
+아기 상어의 크기는 2
+자신의 크기보다 큰 물고기가 있는 칸은 지나갈 수 없다.
+아기 상어는 자신의 크기보다 작은 물고기만 먹을 수 있다.
+- 하지만 크기가 같다면 통과는 가능하다.
+- 자신의 크기만큼 물고기를 먹으면 크기가 증가한다.
+- 물고기가 한마리면 그 물고기를 먹으러 간다.
+- 거리가 같다면 가장 위에 있는 물고기, 가장 왼쪽에 있는 물고기 순이다.
  */
 public class 아기상어_16236 {
 
-    private static int n;
-    private static int start_x;
-    private static int start_y;
-    private static int weight;
     private static int map[][];
-    private static int dx[] = {0, -1 , 1, 0}; //상, 좌, 우 , 하
-    private static int dy[] = {-1, 0, 0, 1};
-    private static ArrayList<int[]> fish_location;
+    private static int dx[] = {-1, 0, 1, 0}; //좌,상,우,하
+    private static int dy[] = {0, -1, 0, 1}; //좌,상,우,하
+    private static int n;
+    private static int shark_size = 2;
+    private static int count = 0;
+    private static boolean visited[][];
+    private static int targetY = 0,targetX = 0;
+    private static ArrayList<int[]> fish;
 
-    private static int calDisTance(int x, int y, int target_x, int target_y) {
+    private static int bfs(int start_y, int start_x, int target_y, int target_x) {
 
-        int result = 0;
         Queue<int[]> q = new LinkedList<>();
-        boolean[][] visited = new boolean[n][n];
-        q.add(new int[]{y,x,0});
+        q.add(new int[]{start_y,start_x,0});
+        int result = 0;
         while(!q.isEmpty()){
-            int[] temp = q.poll();
-            x = temp[0];
-            y = temp[1];
-            int level = temp[2];
-            if(x == target_x && y == target_y){
-                result = level;
-                break;
-            }
+            int[] location = q.poll();
+            int y = location[0];
+            int x = location[1];
+            int level = location[2];
 
-            for(int i = 0; i < 4; i++){
-                int next_x = x + dx[i];
+            for(int i = 0 ; i < 4; i++){
                 int next_y = y + dy[i];
-
-                if(next_x < 0 || next_y < 0 || next_x >= n || next_y >= n )
+                int next_x = x + dx[i];
+                if(next_y < 0 || next_x < 0 || next_y >= n || next_x >= n || visited[next_y][next_x])
                     continue;
-
-                if(map[next_y][next_x] > weight && !visited[next_y][next_x])
+                if(map[next_y][next_x] > shark_size)
                     continue;
-
+                if(next_x == target_x && next_y == target_y){
+                    count++;
+                    map[next_y][next_x] = 0;
+                    if(count == shark_size) {
+                        shark_size++;
+                        count = 0;
+                    }
+                    result = level + 1;
+                    q.clear();
+                    break;
+                }
                 visited[next_y][next_x] = true;
-                q.add(new int[]{next_y, next_x, level + 1});
+                q.add(new int[]{next_y, next_x , level + 1});
             }
-            System.out.println(q.size());
         }
         return result;
     }
 
-    private static int searchFish(int x, int y) {
+    private static boolean check(int start_y, int start_x) {
 
-        //조건(위치, 크기)를 만족하는 물고기의 위치 찾기
-        /**
-         * 조건 1. 거리가 제일 가까운 것
-         * 조건 2. 거리가 가까운 것 중에서 제일 위, 위도 같으면 제일 좌인 것
-         */
-        int min = 1000000;
-        int pre_x = 10000;
-        int pre_y = 10000;
-        for(int[] location : fish_location){
-            int fish_y = location[0];
-            int fish_x = location[1];
-            int distance = calDisTance(x,y,fish_x,fish_y);
-            if(min > distance){
-                min = distance;
-                pre_x = fish_x;
-                pre_y = fish_y;
-            //거리가 제일 가깝다면 제일 위에 있는 좌표
-            }else if(min == distance){
+        int minD = 10000000;
+        int minX = 10000000;
+        int minY = 10000000;
+        if(fish.isEmpty())
+            return false;
 
-                if(fish_y == pre_y){
-                    pre_x = fish_x;
-                }else{
-                    pre_y = fish_y;
-                }
-            }
-        }
-        remove_location(pre_y,pre_x);
-
-        return min;
-    }
-
-    private static int calTime() {
-
-        int time = 0;
-
-        //물고기가 없는 경우
-        while(!fish_location.isEmpty()){
-            display();
-            //물고기가 한마리만 남았을 경우
-            if(fish_location.size() == 1) {
-                //무게 비교 후에 갈 수 있다면 거리만큼 시간 늘리기
-                for(int i = 0; i < n; i++){
-                    for(int j = 0; j < n; j++){
-                        if(map[i][j] > 0 && map[i][j] < 9) {
-                            time += calDisTance(start_x, start_y, j, i);
-                            remove_location(j, i);
-                            return time;
+        int idx = -1;;
+        for(int i = 0; i < fish.size(); i++){
+            int fish_y = fish.get(i)[0];
+            int fish_x = fish.get(i)[1];
+            int distance = Math.abs(start_x - fish_x) + Math.abs(start_y - fish_y);
+            //System.out.println(start_y + " " + start_x + " " + fish_y + " " + fish_x + " "+ shark_size);
+            if(shark_size > map[fish_y][fish_x]) {
+                if (minD > distance) {
+                    minD = distance;
+                    minY = fish_y;
+                    minX = fish_x;
+                    idx = i;
+                }else if( minD == distance){
+                    if(minY > fish_y){
+                        minY = fish_y;
+                        minX = fish_x;
+                        idx = i;
+                    }else if(minY == fish_y){
+                        if(minX > fish_x){
+                            minX = fish_x;
+                            idx = i;
                         }
                     }
                 }
             }
-            //조건(위치, 크기)을 만족하는 물고기의 위치 찾기
-            time += searchFish(start_x,start_y); //가야할 물고기 위치 좌표 반환 하기
+        }
+        if(idx > -1) {
+            targetY = minY;
+            targetX = minX;
+            fish.remove(idx);
+            return true;
+        }
+        return false;
+    }
+
+    private static int search(int start_y, int start_x) {
+        int result = 0;
+        int target_x = -1;
+        int target_y = -1;
+        while(true) {
+            //배열 초기화
+            for (int j = 0; j < n; j++)
+                Arrays.fill(visited[j], false);
+
+            //먹거리가 없으면 break;
+            if(check(start_y,start_x)){
+                target_y = targetY;
+                target_x = targetX;
+            }else{
+                break;
+            }
+
+            //최단거리 탐색
+            result += bfs(start_y, start_x, target_y, target_x);
+            //System.out.println(target_y + " " + target_x +" " +result);
+            //display();
+            start_x = target_x;
+            start_y = target_y;
         }
 
-        return time;
+        return result;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(input.readLine());
+        map = new int[n][n];
+        visited = new boolean[n][n];
+        fish = new ArrayList<>();
+        int start_x = 0;//아기상어 초반 위치
+        int start_y = 0;
+
+        for(int i = 0; i < n; i++){
+            StringTokenizer st = new StringTokenizer(input.readLine());
+            for(int j = 0; j < n; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if(map[i][j] == 9) {
+                    map[i][j] = 0;
+                    start_y = i;
+                    start_x = j;
+                }
+                if(map[i][j] >= 1 && map[i][j] <= 6)
+                    fish.add(new int[]{i,j});
+            }
+        }
+        System.out.println(search(start_y,start_x));
     }
 
     private static void display() {
@@ -159,47 +203,5 @@ public class 아기상어_16236 {
             sb.append("\n");
         }
         System.out.println(sb);
-        System.out.println(fish_location.size());
-    }
-
-    private static void remove_location(int next_y, int next_x) {
-        int result = 0;
-        int idx = 0;
-        for(int[] location: fish_location){
-            if(location[0] == next_y && location[1] == next_x) {
-                result = idx;
-                break;
-            }
-            idx++;
-        }
-        fish_location.remove(result);
-    }
-
-    public static void main(String[] args) throws Exception{
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(input.readLine());
-        map = new int[n][n];
-        fish_location = new ArrayList<>();
-        weight = 2;
-
-        for(int i = 0; i < n; i++){
-            StringTokenizer st = new StringTokenizer(input.readLine());
-            for(int j = 0; j < n; j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
-
-                //물고기의 좌표와 크기
-                if(map[i][j] > 0 && map[i][j] < 9)
-                    fish_location.add(new int[]{i,j});
-
-                //아기 상어 초기 위치
-                if(map[i][j] == 9){
-                    start_x = j;
-                    start_y = i;
-                    map[i][j] = 0;
-                }
-            }
-        }
-
-        System.out.println(calTime());
     }
 }

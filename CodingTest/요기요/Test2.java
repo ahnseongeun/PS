a01392bf3b3fc92c8dc15b7b00e25c722d871609
ahnseongeun <ast3138@naver.com>
2021-06-20 20:25:51 +0900

    2021-6-20 요기오 코테

--- CodingTest/요기요/Test2.java
+++ CodingTest/요기요/Test2.java
@@ -23,8 +23,8 @@ public class Test2 {
             for(int i = 0; i < numbers.length - 1; i++){
                 int ch1 = numbers[i] - '0';
                 int ch2 = numbers[i + 1] - '0';
+                if(ch1 == 0) continue;
                 if(ch1 == ch2){
-                    if(ch1 == 0) continue;
                     if(ch1 == 9){
                         int num = 0;
                         for(int j = i ; j < numbers.length; j++){

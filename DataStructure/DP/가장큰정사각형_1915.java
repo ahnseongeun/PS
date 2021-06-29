80f911c18b00fc8f7e5cb708b7d1b6a21f32014b
ahnseongeun <ast3138@naver.com>
2021-06-29 16:43:13 +0900

    2021-6-29 가장큰정사각형

---
+++ DataStructure/DP/가장큰정사각형_1915.java
@@ -0,0 +1,51 @@
+package DataStructure.DP;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+4 4
+0100
+0111
+1110
+0010
+ */
+
+/**
+ * n,m이 1000이라서 완전탐색을 사용할 경우
+ * 1000 * 1000의 배열을 1000*1000*(1000000)
+ */
+public class 가장큰정사각형_1915 {
+
+    private static int getMaxSquare(int n, int m, int[][] arr) {
+
+        int result = 0;
+        int[][] dp = new int[n + 1][m + 1];
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(arr[i - 1][j - 1] == 0) dp[i][j] = 0;
+                else {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j],dp[i - 1][j - 1]),dp[i][j - 1]) + 1;
+                    result = Math.max(result,dp[i][j]);
+                }
+            }
+        }
+        return result * result;
+    }
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(input.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n][m];
+        for(int i = 0; i < n; i++){
+            String str = input.readLine();
+            for(int j = 0; j < m; j++){
+                arr[i][j] = (str.charAt(j) - '0');
+            }
+        }
+        System.out.println(getMaxSquare(n,m,arr));
+    }
+}
--- DataStructure/진행중인문제/박스채우기_1493.java
+++ DataStructure/진행중인문제/박스채우기_1493.java
@@ -20,46 +20,49 @@ import java.util.TreeMap;
  */
 public class 박스채우기_1493 {
 
+    private static int[] box;
     private static int result = 0;
     private static boolean check = false;
-    private static void fillCube(int length, int width, int height, TreeMap<Integer, Integer> box) {
 
-        if(length == 0 || width == 0 || height == 0) return;
-        for(int idx : box.keySet()){
-            //길이가 idx인 큐브의 크기
-            int cubeCount = box.get(idx);
-            int cubeLen = 1 << idx;
-            if(cubeLen > 0 && length - cubeLen >= 0 || width - cubeLen >= 0 || height - cubeLen >= 0) {
-                box.put(idx, cubeCount - 1);
-                result++;
-                fillCube(length - cubeLen, width, height, box);
-                fillCube(length, width - cubeLen, height, box);
-                fillCube(length, width, height - cubeLen, box);
-                return;
-            }
+    private static void fillCube(int length, int width, int height, int idx) {
+
+        if(idx == -1) {
+            if (length * width * height > 0) check = true;
+            return;
         }
-        check = true;
+            //길이가 idx인 큐브의 크기
+        int cubeCount = box[idx];
+        int cubeLen = (1 << idx);
+        if(cubeCount < 1 || length - cubeLen < 0 || width - cubeLen < 0 || height - cubeLen < 0) {
+            fillCube(length, width, height, idx - 1);
+            //3가지 방향으로 채우기
+            return;
+        }
+        box[idx]--;
+        result++;
+        fillCube(length - cubeLen, width, height, idx );
+        fillCube(length, width - cubeLen, height, idx );
+        fillCube(length, width, height - cubeLen, idx );
     }
 
     public static void main(String[] args) throws Exception{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(input.readLine());
-        TreeMap<Integer, Integer> box = new TreeMap<>((o1, o2) -> o2 - o1);
         int length = Integer.parseInt(st.nextToken());
         int width = Integer.parseInt(st.nextToken());
         int height = Integer.parseInt(st.nextToken());
 
         int n = Integer.parseInt(input.readLine());
 
-        for(int i = 0; i < n; i++){
+        box = new int[n];
+        for(int i = n - 1; i >= 0; i--){
             st = new StringTokenizer(input.readLine());
             int idx = Integer.parseInt(st.nextToken());
             int cnt = Integer.parseInt(st.nextToken());
-            box.put(idx,cnt);
+            box[idx] = cnt;
         }
 
-        fillCube(length, width, height, box);
-        System.out.println(check);
+        fillCube(length, width, height,n - 1);
         System.out.println(result);
     }
 }

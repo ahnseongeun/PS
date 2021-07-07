e21178bca87c1898b6fe7dc55b5ed2c960d8cbaf
ahnseongeun <ast3138@naver.com>
2021-07-07 15:30:27 +0900

    2021-7-7 A와B

---
+++ DataStructure/진행중인문제/효율적인해킹_1325.java
@@ -0,0 +1,22 @@
+package DataStructure.진행중인문제;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+5 4
+3 1
+3 2
+4 3
+5 3
+ */
+public class 효율적인해킹_1325 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(input.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+    }
+}

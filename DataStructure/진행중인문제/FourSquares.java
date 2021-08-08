6f29ca6e67c52a65b7ff391fdd67ec9ced9a0981
ahnseongeun <ast3138@naver.com>
2021-08-09 00:45:24 +0900

    2021-8-8 FourSquares

--- DataStructure/진행중인문제/FourSquares.java
+++ DataStructure/진행중인문제/FourSquares.java
@@ -2,19 +2,26 @@ package DataStructure.진행중인문제;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class FourSquares {
     public static void main(String[] args) throws Exception{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(input.readLine());
-        int cnt = 0;
-        while(n != 0) {
-            int num = (int) Math.sqrt(n);
-            n -= (num * num);
-            System.out.println(n);
-            cnt++;
+        int[] dp = new int[50001];
+        Arrays.fill(dp, 1000000);
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 1; i <= 50000; i++) {
+            for(int j = 1; j <= Math.sqrt(i); j++) {
+                if(i - (j * j ) >= 0) {
+                    dp[i] = Math.min(dp[i], dp[i - (j * j)] + 1);
+                }
+            }
         }
-        System.out.println(cnt);
+
+        System.out.println(dp[n]);
     }
 }

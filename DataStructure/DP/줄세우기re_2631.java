e360869fdc2164fd4a7fd99bdf0dcbdfc1782896
ahnseongeun <ast3138@naver.com>
2021-08-20 17:49:55 +0900

    2021-8-20 줄세우기

---
+++ DataStructure/DP/줄세우기re_2631.java
@@ -0,0 +1,39 @@
+package DataStructure.DP;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/*
+7
+3
+7
+5
+2
+6
+1
+4
+ */
+public class 줄세우기re_2631 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(input.readLine());
+        int[] arr = new int[n + 1];
+        int[] dp = new int[n + 1];
+        for(int i = 1; i <= n; i++) arr[i] = Integer.parseInt(input.readLine());
+        Arrays.fill(dp,1);
+
+        int max = 0;
+        //최장 증가하는 수열 구하기
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j < i; j++) {
+                if(arr[j] < arr[i] && dp[j] >= dp[i]) {
+                    dp[i] = dp[j] + 1;
+                    max = Math.max(max, dp[i]);
+                }
+            }
+        }
+        System.out.println(n - max);
+    }
+}

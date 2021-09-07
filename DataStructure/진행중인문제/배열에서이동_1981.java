a06d16379020f0414af7a78d4842fd1fb90edafd
ahnseongeun <ast3138@naver.com>
2021-09-07 21:25:47 +0900

    2021-9-7 배열에서 이동

--- DataStructure/진행중인문제/배열에서이동_1981.java
+++ DataStructure/진행중인문제/배열에서이동_1981.java
@@ -2,9 +2,7 @@ package DataStructure.진행중인문제;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 /*
 5
@@ -13,61 +11,105 @@ import java.util.StringTokenizer;
 4 4 0 3 3
 8 0 2 3 4
 4 3 0 2 1
+
+2
+1 2
+3 4
  */
 
 /**
  * 완전 탐색을 할 경우 시간복잡도는?
- * 1. 모든 인덱스를 담은 배열을 만든다.
- * 2. 이분 탐색으로 최댓값과 최솟값을 이용해서 (최댓값 - 최솟값)을 찾는다.
- * 3. 해당 차를 기준으로 BFS를 통해서 1,1에서 N - 1, N - 1으로 갈 수 있는지 찾는다.
+ * 탐색의 마지막 인덱스는 배열의 (최대값 - 최소값)
+ *
+ * https://gre-eny.tistory.com/304
+ * https://hsdevelopment.tistory.com/549
  */
 public class 배열에서이동_1981 {
 
     static int[][] arr;
-    static int[][] visited;
+    static boolean[][] visited;
     static int[] dx = {0, 1, 0, -1}; //상, 우, 하, 좌
     static int[] dy = {-1, 0, 1, 0}; //상, 우, 하, 좌
     static int n;
+    static Set<Integer> set = new HashSet<>();
+    static ArrayList<Integer> list = new ArrayList<>();
 
-    static void bfs(int y, int x) {
+    static boolean bfs(int minValue, int maxValue) {
 
+        if (arr[0][0] < minValue || arr[0][0] > maxValue) {
+            return false;
+        }
+
+        visited = new boolean[n][n];
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{y, x, arr[y][x]}); // y, x, max 값, max - min의 최소값
-        visited[y][x] = 1000000000;
+        q.add(new int[]{0, 0}); // y, x, max 값, max - min의 최소값
+        visited[0][0] = true;
 
         while(!q.isEmpty()) {
 
             int[] node = q.poll();
-            y = node[0];
-            x = node[1];
-            int maxValue = node[2];
+            int y = node[0];
+            int x = node[1];
 
-            if( y == n - 1 && x == n - 1) {
-                continue;
+            if (y == n - 1 && x == n - 1) {
+                return true;
             }
-            for(int i = 0; i < 4; i++) {
+            for (int i = 0; i < 4; i++) {
                 int next_y = y + dy[i];
                 int next_x = x + dx[i];
-                if(next_y < 0 || next_x < 0 || next_y >= n || next_x >= n) continue;
-                if(!check()) continue;
-                visited[y][x] = maxValue - arr[y][x];
-                q.add(new int[]{next_y, next_x, maxValue});
+                if (next_y < 0 || next_x < 0 || next_y >= n || next_x >= n || visited[next_y][next_x]) continue;
+                if (arr[next_y][next_x] < minValue || arr[next_y][next_x] > maxValue) continue;
+                visited[next_y][next_x] = true;
+                q.add(new int[]{next_y, next_x});
             }
         }
+
+        return false;
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(input.readLine());
         arr = new int[n][n];
-        visited = new int[n][n];
+
         for(int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(input.readLine());
             for(int j = 0; j < n; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
+                set.add(arr[i][j]);
             }
         }
 
-        bfs(0,0);
+
+        list.addAll(set);
+        Collections.sort(list);
+
+        //중복없이 오름차순 정렬
+
+        int idx1 = 0;
+        int idx2 = 0;
+        int end = list.size();
+        int result = 201;
+        //투포인터를 통해서 모든 MAX - MIN 값이 가능한 수들 중에서 최솟값을 찾는다.
+        while(idx1 < end && idx2 < end) {
+
+            int minValue = list.get(idx1);
+            int maxValue = list.get(idx2);
+
+            //(MAX - MIN)이 가능한 배열의 원소라는 말은 해당 원소가 MAX보다 크고 MIN보다 작다는 말이다.
+            boolean check = bfs(minValue, maxValue);
+
+            //check = true라면 끝까지 도달 했다는 것
+            if(check) {
+                result = Math.min(result, maxValue - minValue);
+
+                //도달 가능하면 idx1++을 통해서 MIN값을 증가시켜서 (MAX - MIN)값을 감소시킨다.
+                idx1++;
+            } else {
+                idx2++;
+            }
+        }
+        System.out.println(result);
+
     }
 }

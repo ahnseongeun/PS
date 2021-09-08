17c2004004bd1f0259d9a55091f20c0e5d0077a3
ahnseongeun <ast3138@naver.com>
2021-09-08 21:37:15 +0900

    2021-9-8 기상캐스터

---
+++ DataStructure/진행중인문제/기상캐스터_10709.java
@@ -0,0 +1,50 @@
+package DataStructure.진행중인문제;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+3 4
+c..c
+..c.
+....
+ */
+public class 기상캐스터_10709 {
+
+    static char[][] arr;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(input.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        arr = new char[n][m];
+
+        for(int i = 0; i < n; i++) {
+            String str = input.readLine();
+            for(int j = 0; j < m; j++) {
+                arr[i][j] = str.charAt(j);
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            int value = -1;
+            for(int j = 0; j < m; j++) {
+                if( arr[i][j] == '.') {
+                    if(value == -1) sb.append(value).append(" ");
+                    else sb.append(value++).append(" ");
+                }
+                if( arr[i][j] == 'c') {
+                    value = 0;
+                    sb.append(value++).append(" ");
+                }
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+}

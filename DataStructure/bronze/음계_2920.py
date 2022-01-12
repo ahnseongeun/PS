745b2d80ee54d81e88bf4965687827809862a610
ahnseongeun <ast3138@naver.com>
2022-01-13 00:40:44 +0900

    2022-01-12 파이썬 알고리즘

--- DataStructure/bronze/음계_2920.py
+++ DataStructure/bronze/음계_2920.py
@@ -1,27 +1,9 @@
 arr = list(map(int, input().split()))
-firstValue = arr[0]
-flag = 0
-if firstValue == 1:
-    flag = 0
-    for i in range(1, 8):
-        if i == arr[i - 1]:
-            continue
-        flag = 2
-        break
-elif firstValue == 8:
-    flag = 1
-    for i in range(8, 1, -1):
-        if i == arr[i - 1]:
-            continue
-        flag = 2
-        break
-else:
-    flag = 2
 
-if int(flag) == 0:
+if arr == sorted(arr):
     print("ascending")
-elif int(flag) == 1:
+elif arr == sorted(arr, reverse = True):
     print("descending")
-if int(flag) == 2:
+else:
     print("mixed")
 

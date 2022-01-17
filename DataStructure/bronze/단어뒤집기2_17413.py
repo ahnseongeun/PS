ac9293ed023d67fedc13cb7fcc9a74eddda1fc3d
ahnseongeun <ast3138@naver.com>
2022-01-18 00:15:57 +0900

    2022-01-17 파이썬 알고리즘

--- DataStructure/bronze/단어뒤집기2_17413.py
+++ DataStructure/bronze/단어뒤집기2_17413.py
@@ -1,16 +1,39 @@
 string = input()
 
-result = ""
-word = ""
+result = []
+stack = []
 reverseFlag = True
 
 for ch in string:
+
+    #정방향
     if ch == '<':
         reverseFlag = False
-        result += ch
+        stack.append(ch)
+    #스택에 있는거 result에 넣어주기
     elif ch == '>':
         reverseFlag = True
-        result
+        stack.append(ch)
+        result += stack
+        stack = []
+    #<word>test test 방지 -> ' 'result에 넣어주기
+    elif ch == ' ':
+        stack.append(ch)
+        result += stack
+        stack = []
+    else:
+        if reverseFlag:
+            stack.insert(0, ch)
+        elif not reverseFlag:
+            stack.append(ch)
+
+result += stack
+print(''.join(result))
+
+
+
+
+
 
 
 

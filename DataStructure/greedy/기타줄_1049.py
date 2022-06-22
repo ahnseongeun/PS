02b31d1ca16c293c06051067de912732d0dee2af
ahnseongeun <ast3138@naver.com>
2022-06-23 01:29:14 +0900

    2022-06-23 기타줄

--- DataStructure/greedy/기타줄_1049.py
+++ DataStructure/greedy/기타줄_1049.py
@@ -7,4 +7,11 @@ for i in range(m):
         minPackage = p
     if minCount > v:
         minCount = v
-print(n // 6 * minPackage + n % 6 * minCount)
+
+if minPackage < minCount * 6:
+    if minPackage < n % 6 * minCount:
+        print(n//6 * minPackage + minPackage)
+    else:
+        print(n//6 * minPackage + n%6 * minCount)
+elif minPackage >= minCount * 6:
+    print(n * minCount)
